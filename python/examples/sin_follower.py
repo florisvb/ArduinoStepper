307c37a1791a9768381a2df9937bef062baf0d98
florisvb <florisvb@gmail.com>
2013-01-30 16:02:22 -0800

    added sin follower that uses position control function

--- python/examples/sin_follower.py
+++ python/examples/sin_follower.py
@@ -3,7 +3,7 @@ import time
 import numpy as np
 import arduino_stepper.arduino_stepper as arduino_stepper
 
-def sin_curve_in_position(amplitude, frequency, time_to_wiggle_for=5, maxvel=5000, acceptable_error=2, gain_proportional=10, gain_integral=1, port='/dev/ttyACM0', baudrate=57600, timeout=1):
+def sin_curve_in_position_with_vel_control(amplitude, frequency, time_to_wiggle_for=5, maxvel=5000, acceptable_error=2, gain_proportional=10, gain_integral=1, port='/dev/ttyACM0', baudrate=57600, timeout=1):
     '''
     
     '''
@@ -40,7 +40,44 @@ def sin_curve_in_position(amplitude, frequency, time_to_wiggle_for=5, maxvel=500
     ax.set_ylabel('value')
     
     
+def sin_curve_in_position_with_pos_control(amplitude, frequency, time_to_wiggle_for=5, maxvel=1000, port='/dev/ttyACM0', baudrate=57600, timeout=1):
+    '''
+    
+    '''
+    def sinfunc(t):
+        return amplitude*np.sin( t*2*np.pi*frequency )
+    
+    astep = arduino_stepper.Arduino_Stepper(port=port,timeout=timeout, baudrate=baudrate)
+    astep.reset_step_counter()
+
+    time_start = time.time()
+    t = time.time() - time_start
+    
+    desired_position_array = []
+    desired_position_time = []
+    actual_position_array = []
+    actual_position_time = []
+    
+    while t < time_to_wiggle_for:
+        t = time.time() - time_start
+        desired_position = sinfunc(t)
+        desired_position_array.append(desired_position)
+        desired_position_time.append(t)
+        pos = astep.go_to_pos(desired_position, maxvel)
+        actual_position_array.append(pos)
+        actual_position_time.append(time.time()-time_start)
+        
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    
+    ax.plot(desired_position_time, desired_position_array, 'black')
+    ax.plot(actual_position_time, actual_position_array, 'red')
+        
+    ax.set_xlabel('time, sec')
+    ax.set_ylabel('value')
+    
+    
 if __name__ == '__main__':
     amplitude = 1000 # in steps
     frequency = 0.1 # in steps per second
-    sin_curve_in_position(amplitude, frequency, time_to_wiggle_for=10, port='/dev/ttyACM0', baudrate=57600, timeout=1)
+    sin_curve_in_position_with_pos_control(amplitude, frequency, time_to_wiggle_for=10, port='/dev/ttyACM0', baudrate=57600, timeout=1)
